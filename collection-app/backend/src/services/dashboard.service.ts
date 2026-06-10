909ea9b111e98e48e6eadf18d7d9e7fdd71ba0f4
sera.george <sera.george@qburst.com>
2026-06-10 13:09:03 +0530

    feat: dashboard needs active packages record, active means ones that are in motion that includes pickedup and in-transit ones

--- collection-app/backend/src/services/dashboard.service.ts
+++ collection-app/backend/src/services/dashboard.service.ts
@@ -2,33 +2,35 @@ import { PackageStatus } from "@/generated/prisma/enums";
 import { prisma } from "@/lib/prisma";
 
 export const getDashboardData = async () => {
-  const [pendingPackages, transitedPackages, delayedPackages] =
-    await Promise.all([
-      prisma.package.findMany({
-        where: {
-          status: PackageStatus.TO_BE_PICKED_UP,
-        },
-      }),
-      prisma.package.findMany({
-        where: {
-          status: PackageStatus.IN_TRANSIT,
-        },
-      }),
-      prisma.package.findMany({
-        where: {
-          status: PackageStatus.DELAYED,
-        },
-      }),
-    ]);
+  const [pendingPackages, activePackages, delayedPackages] = await Promise.all([
+    prisma.package.findMany({
+      where: {
+        status: PackageStatus.TO_BE_PICKED_UP,
+      },
+      orderBy: { createdAt: "desc" },
+    }),
+    prisma.package.findMany({
+      where: {
+        status: { in: [PackageStatus.IN_TRANSIT, PackageStatus.PICKED_UP] },
+      },
+      orderBy: { createdAt: "desc" },
+    }),
+    prisma.package.findMany({
+      where: {
+        status: PackageStatus.DELAYED,
+      },
+      orderBy: { createdAt: "desc" },
+    }),
+  ]);
 
   return {
     pendingPackages: {
       count: pendingPackages.length,
       packages: pendingPackages,
     },
-    transitedPackages: {
-      count: transitedPackages.length,
-      packages: transitedPackages,
+    activePackages: {
+      count: activePackages.length,
+      packages: activePackages,
     },
     delayedPackages: {
       count: delayedPackages.length,
