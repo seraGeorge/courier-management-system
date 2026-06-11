9e800b88e5feeb8d2b1a4ecad92ff987ce3e5cec
sera.george <sera.george@qburst.com>
2026-06-11 16:46:58 +0530

    chore: include relevant details of package to be passed in case of tracking

--- collection-app/backend/src/services/track.service.ts
+++ collection-app/backend/src/services/track.service.ts
@@ -5,6 +5,16 @@ export const trackPackage = async (trackingId: string) => {
     where: {
       trackingId,
     },
-    include: { sale: true },
+    select: {
+      trackingId: true,
+      status: true,
+      delayReason: true,
+      region: {
+        select: { code: true, name: true },
+      },
+      sale: {
+        select: { amount: true, createdAt: true },
+      },
+    },
   });
 };
